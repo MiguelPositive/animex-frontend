3ed9ca6b9296b65d47ad32a043f7881976a56e32
miguel angel clavijo moreno <miguelpositive@gmail.com>
2023-06-02 14:25:53 -0500

    ajusto la dimenciones de la imagen de la card de anime

--- src/components/animes/Animes.jsx
+++ src/components/animes/Animes.jsx
@@ -4,37 +4,33 @@ import { store } from "../context/ContextApp";
 import { useImage } from "react-image";
 
 const Animes = () => {
-  // const { src } = useImage({
-  //   srcList: "https://www.example.com/foo.jpg",
-  // });
-
   const { animesCopy } = useContext(store);
 
   return (
     <div className="flex justify-center gap-5 flex-wrap ">
       {animesCopy.map((anime) => (
         <div
-          className={`w-[15rem] h-[20rem] bg-white rounded-md cursor-pointer overflow-hidden transition-all duration-200 hover:scale-110`}
+          className={`relative w-[14rem] h-[20rem] bg-white rounded-md cursor-pointer overflow-hidden transition-all duration-200 hover:scale-110 z-10`}
           key={anime._id}
         >
-          <div id="img-anime" className="w-[15rem] h-[10rem]">
-            <img
-              src={`https://animex-backend.onrender.com/imgsAnime/${anime.poster}`}
-              alt={`Poster ${anime.name}`}
-              width="100%"
-              height="100%"
-            />
-          </div>
           <div
             id="text-anime"
-            className="w-full flex flex-wrap justify-center items-center text-black h-[5rem]"
+            className="w-full flex flex-wrap justify-center items-center text-black h-[5rem] "
           >
-            <p className="w-full text-center text-black text-2xl font-bold mt-5 mb-5">
+            <p className="w-full text-center text-black text-2xl font-bold">
               {anime.name}
             </p>
-            <p className="w-full text-justify pl-5 pr-5 pb-5">
-              {anime.description}
-            </p>
+          </div>
+
+          <img
+            id="image-anime"
+            className="-mt-4 w-full h-[17rem]"
+            src={`https://animex-backend.onrender.com/imgsAnime/${anime.poster}`}
+            alt={`Poster ${anime.name}`}
+          />
+
+          <div className="absolute top-0 right-3  text-black">
+            <i class="bi bi-hurricane text-2xl"></i>
           </div>
         </div>
       ))}
